adb7dc36045fd15d42e2c11ff884bb3b552260bf
SoraTheSky <cesardedios94@gmail.com>
2019-01-14 20:34:14 -0500

    Script debug needed

--- VarScope/scope.js
+++ VarScope/scope.js
@@ -1,1 +1,22 @@
 var global = "This is Worldwide Global News<hr>";
+
+function us(obj) 
+{
+    var local = "***This is United states Local news***<hr>";
+    obj.innerHTML += local; obj.innerHTML += global;
+}
+
+function eu(obj) 
+{
+    var local = "---This is European Local News---<hr>"
+    obj.innerHTML += local; obj.innerHTML += global;    
+}
+
+function init() 
+{
+    var obj = document.getElementById("panel");
+    obj.innerHTML = global;
+    us( obj );
+    us( obj );
+}
+document.addEventListener("DOMConetenLoaded", init, false)
