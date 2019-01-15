4dc139d0c258c31c5a5eb5b366e70fa44dd74065
SoraTheSky <cesardedios94@gmail.com>
2019-01-15 12:06:21 -0500

    Finished the script structure to present data

--- AssessLogic/logic.js
+++ AssessLogic/logic.js
@@ -1,5 +1,17 @@
 function init() {
-    
+    var yes = true, no = false;
+    var blnA = yes && yes;
+    var blnB = yes && no;
+    var blnC = no || yes;
+    var blnD = no || no;
+    var tog = !yes;
+
+    var str = "Are both values true?: " + blnA + "<br>";
+    str += "Are both values ture now?: " + blnB + "<br>";
+    str += "Is either value true?: " + blnC + "<br>";
+    str += "Is either value true now?: " + blnD + "<br>";
+    str += "Initial value: " + yes + "<br>";
+    str += "Toggled value: " + tog + "<br>";
     document.getElementById("panel").innerHTML = str;
 }
 document.addEventListener("DOMContentLoaded", init, false )
