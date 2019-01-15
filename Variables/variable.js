b28482946af46b5e3a0ac721c577dfa91ac6509c
SoraTheSky <cesardedios94@gmail.com>
2019-01-14 20:01:48 -0500

    Finished variable typing

---
+++ Variables/variable.js
@@ -0,0 +1,15 @@
+function init() 
+{
+    var str = "Text Conent in JavaScript";
+    var num = 100;
+    var bln = true;
+    var fcn = init;
+    var obj = document.getElementById("panel");
+
+    obj.innerHTML = str + ": " + typeof str;
+    obj.innerHTML += "<br>" + num + ": " + typeof num;
+    obj.innerHTML += "<br>" + bln + ": " + typeof bln;
+    obj.innerHTML += "<br>init(): " + typeof fcn;
+    obj.innerHTML += "<br>" + obj + ": " + typeof obj;
+}
+document.addEventListener("DOMContentLoaded", init, false);
