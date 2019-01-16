723117c44a8d86893e9220d1d3137ecafbc931f0
SoraTheSky <cesardedios94@gmail.com>
2019-01-16 17:21:25 -0500

    forgot to append the accelerate to the pannel with +=

--- ObjectManipulation/CreatingObjects/object.js
+++ ObjectManipulation/CreatingObjects/object.js
@@ -1,8 +1,5 @@
 
-function accelerate() 
-{
-    return "<br>Vroom!"    
-}
+function accelerate(){return "<br>Vroom!";}
 
 function Car(make, model, color) 
 {
@@ -17,7 +14,7 @@ function init()
     var panel = document.getElementById("panel");
     var myCar = new Car("Dodge", " Viper", "Red ");
 
-    panel.innerHTML = myCar.color + myCar.make + myCar.model;
-    panel.innerHTML = myCar.accelerate();
+    panel.innerHTML = myCar.color+ myCar.make+ myCar.model;
+    panel.innerHTML += myCar.accelerate();
 }
 document.addEventListener("DOMContentLoaded", init, false)
