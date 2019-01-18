43f510aa9ca0c893121c9f8dc5a9bd5eb94c181d
SoraTheSky <cesardedios94@gmail.com>
2019-01-18 16:14:13 -0500

    set up variables and init function

---
+++ ObjectManipulation/OArrayPropMethod/slice.js
@@ -0,0 +1,6 @@
+function init()
+{
+    var panel = document.getElementById("panel");
+    var seasons = ["Spring", "Summer", "Fall", "Winter"]
+}
+document.addEventListener("DOMContentLoaded", init, false)
